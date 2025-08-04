e61b7c212a3124349b570632bedc24bf41372d4b
Bruno Santos <bms.santos@outlook.pt>
2025-08-04 15:06:42 +0100

    [init] First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+<img src="https://i.imgur.com/U38Palm.jpeg" width="100%">
+<img src="https://i.imgur.com/j461bpg.jpeg" width="100%">
---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+import "@/app/styles/globals.css";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
