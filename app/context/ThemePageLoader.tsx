ce649a265345fe854e26a4d2579a17bffbdec64d
morangoo <miguel.bms@outlook.pt>
2025-08-04 23:18:58 +0100

    [update] New loader.

--- app/context/ThemePageLoader.tsx
+++ app/context/ThemePageLoader.tsx
@@ -9,15 +9,47 @@ type Props = {
 export default function ThemedPageLoader({ route }: Props) {
   const { theme } = useTheme();
   const [ThemePage, setThemePage] = useState<React.ComponentType | null>(null);
+  const [isClient, setIsClient] = useState(false);
+
 
   useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  useEffect(() => {
+    if (!isClient) return;
+    setThemePage(null);
     import(`../layouts/${theme}/${route}`)
       .then((module) => setThemePage(() => module.default))
       .catch((error) => {
         console.error("Error loading themed page:", error);
         setThemePage(null);
       });
-  }, [theme, route]);
+  }, [theme, route, isClient]);
 
-  return ThemePage ? <ThemePage /> : <div>Loading...</div>;
+  if (!isClient) return null;
+
+  if (!ThemePage) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "100vh",
+          width: "100vw",
+        }}
+      >
+        <img
+          src="https://avatars.githubusercontent.com/u/171176624?v=4"
+          alt="Loading"
+          className="img-spin"
+          width={80}
+          height={80}
+        />
+      </div>
+    );
+  }
+
+  return <ThemePage />;
 }
