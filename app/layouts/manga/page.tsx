8e64fc6524bcaf1d2628b9926cd42ff27134c431
Bruno Santos <bms.santos@outlook.pt>
2025-08-06 17:30:02 +0100

    [add] Added manga theme's pallete to manga.css

--- app/layouts/manga/page.tsx
+++ app/layouts/manga/page.tsx
@@ -2,7 +2,9 @@ import MangaLayout from "./layout";
 export default function MangaTheme() {
     return (
         <MangaLayout>
-            <div>Manga Theme</div>
+            <div style={{ display: "flex", alignItems: "center", justifyContent: "center"}}>
+                <h1 style={{fontSize: "20vh", fontWeight: "bold"}}>Manga Theme</h1>
+            </div>
         </MangaLayout>
     );
 }
