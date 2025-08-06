e6cd4facb34c6a161b05d9056cb36c0551503002
Bruno Santos <bms.santos@outlook.pt>
2025-08-06 17:39:09 +0100

    [stweak] a bit of japanese

--- app/layouts/manga/page.tsx
+++ app/layouts/manga/page.tsx
@@ -3,7 +3,7 @@ export default function MangaTheme() {
     return (
         <MangaLayout>
             <div style={{ display: "flex", alignItems: "center", justifyContent: "center"}}>
-                <h1 style={{fontSize: "20vh", fontWeight: "bold"}}>Manga Theme</h1>
+                <h1 style={{fontSize: "20vh", fontWeight: "bold"}}>漫画 Theme</h1>
             </div>
         </MangaLayout>
     );
